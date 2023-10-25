7a133dbb637f6f8cd212cf2d2ddf4f4ca77a60c7
Ray Liu <wooshadyyy@gmail.com>
2023-10-25 02:29:18 -0400

    getmapping api added

--- src/main/java/com/lhr/studentdemo/student/StudentController.java
+++ src/main/java/com/lhr/studentdemo/student/StudentController.java
@@ -5,6 +5,7 @@ import com.lhr.studentdemo.exception.ApiRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,5 +44,12 @@ public class StudentController  {
   @PostMapping
   public void addNewStudent(@RequestBody @Valid Student student){
     studentService.addNewStudent(student);
+  }
+
+  @GetMapping(path = "{studentId}/courses")
+  public List<StudentCourse> getAllCoursesForStudent(@PathVariable("studentId") UUID studentId){
+    // TO DO
+    System.out.println(studentId);
+    return null;
   }
 }
