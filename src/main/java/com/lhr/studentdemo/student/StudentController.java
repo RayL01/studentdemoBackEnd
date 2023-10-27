589d24b2bb6ae23e67ae4b78f1255521c0f88788
Ray Liu <wooshadyyy@gmail.com>
2023-10-26 23:35:21 -0400

    static resources added

--- src/main/java/com/lhr/studentdemo/student/StudentController.java
+++ src/main/java/com/lhr/studentdemo/student/StudentController.java
@@ -24,7 +24,7 @@ import jakarta.validation.Valid;
  * @Description:
  */
 @RestController
-@RequestMapping("students")
+@RequestMapping("api/students")
 public class StudentController  {
 
   private final StudentService studentService;
